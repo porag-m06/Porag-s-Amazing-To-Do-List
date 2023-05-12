5d152a6b3f098ed86d40f395c6a55c25e7c4bf56
porag-m06 <porag.m06@gmail.com>
2023-05-13 00:16:15 +0600

    loader for css and plugin for HTML auto generation in ./dist are added

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,10 @@
 const myPath = require('path'); //Having the path module to set path for entry & output if we want outside default
+const HtmlWebpackPlugin = require('html-webpack-plugin'); //Plugin to automatically create the index.html file in the /dist directory.
+const { template } = require('lodash');
 
 module.exports = {
+
+    //webpack mode, entry and output config:
     mode: 'development',
     entry: {
         mymain: myPath.resolve(__dirname,'src/index.js')},
@@ -8,5 +12,26 @@ module.exports = {
         path: myPath.resolve(__dirname,'dist'),
         filename:'[name].bundle.js',
         clean:true
-    }
+    },
+
+    //webpack css loaders (style-loder, css-loader) config:
+    module:{
+        rules:[
+            {
+                test: /\.css$/i,
+                use:['style-loader','css-loader'],
+            }
+        ],
+    },
+
+    //webpack Html plugin for auto generating HTML under dist
+    plugins: [
+        new HtmlWebpackPlugin ({
+            template : './src/index.html'
+        }),
+    ],
+
+    
+
+
 }
