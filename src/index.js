7b829671f035cdb07ca7cc09c276857c5f4a318b
porag-m06 <porag.m06@gmail.com>
2023-05-13 15:57:43 +0600

    linter issues fixed

--- src/index.js
+++ src/index.js
@@ -1,42 +1,37 @@
 import './style.css';
-import refresh from  '../assets/rotate-solid.svg';
+import refresh from '../assets/rotate-solid.svg';
 
-const refreshImg = document.getElementById("refresh");
+const refreshImg = document.getElementById('refresh');
 refreshImg.src = refresh;
 
-const todo_tasks = [
+const todoTasks = [
   {
-    description: "wash the dishes",
+    description: 'wash the dishes',
     completed: false,
-    index: 0
+    index: 0,
   },
   {
-    description: "complete To-do list project",
+    description: 'complete To-do list project',
     completed: false,
-    index: 2
+    index: 2,
   },
   {
-    description: "do the daily coding challenges",
+    description: 'do the daily coding challenges',
     completed: false,
-    index: 1
-  }
+    index: 1,
+  },
 ];
 
-todo_tasks.sort((a,b)=>{
-  return a.index - b.index;
-});
+todoTasks.sort((a, b) => a.index - b.index);
 
-const setTodoList = (todo_tasks) => {
-  console.log('before:',todo_tasks);
-  const list = document.querySelector(".list");
-
-  todo_tasks.forEach((task)=>{
-    let listItem = document.createElement('li');
-    listItem.className = "hbb";
+const setTodoList = (todoTasks) => {
+  const list = document.querySelector('.list');
+  todoTasks.forEach((task) => {
+    const listItem = document.createElement('li');
+    listItem.className = 'hbb';
     listItem.innerText = task.description;
     list.appendChild(listItem);
-  })
-
+  });
 };
 
-setTodoList(todo_tasks);
+setTodoList(todoTasks);
