02cd7cd41df634f7b83af8731d4f69f99664e449
porag-m06 <porag.m06@gmail.com>
2023-05-13 15:54:49 +0600

    todo list template with image loading added

--- src/index.js
+++ src/index.js
@@ -1,12 +1,42 @@
-import _ from 'lodash';
-import './style.css'
-function component() {
-    const element = document.createElement('div');
-  
-    //Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack!, From index.js'], ' ');
-    element.classList.add('hello');
-    return element;
+import './style.css';
+import refresh from  '../assets/rotate-solid.svg';
+
+const refreshImg = document.getElementById("refresh");
+refreshImg.src = refresh;
+
+const todo_tasks = [
+  {
+    description: "wash the dishes",
+    completed: false,
+    index: 0
+  },
+  {
+    description: "complete To-do list project",
+    completed: false,
+    index: 2
+  },
+  {
+    description: "do the daily coding challenges",
+    completed: false,
+    index: 1
   }
-  
-  document.body.appendChild(component());
+];
+
+todo_tasks.sort((a,b)=>{
+  return a.index - b.index;
+});
+
+const setTodoList = (todo_tasks) => {
+  console.log('before:',todo_tasks);
+  const list = document.querySelector(".list");
+
+  todo_tasks.forEach((task)=>{
+    let listItem = document.createElement('li');
+    listItem.className = "hbb";
+    listItem.innerText = task.description;
+    list.appendChild(listItem);
+  })
+
+};
+
+setTodoList(todo_tasks);
--- webpack.config.js
+++ webpack.config.js
@@ -1,56 +1,57 @@
-//Install webpack & webpack cli and get the path module and set mode, entry and output
-//npm install webpack webpack-cli --save-dev
+// Install webpack & webpack cli and get the path module and set mode, entry and output
+// npm install webpack webpack-cli --save-dev
 
-const myPath = require('path'); //Having the path module to set path for entry & output if we want outside default
-const HtmlWebpackPlugin = require('html-webpack-plugin'); //Plugin to automatically create the index.html file in the /dist directory.
+const myPath = require('path'); // Having the path module to set path for entry & output if we want outside default
+const HtmlWebpackPlugin = require('html-webpack-plugin'); // Plugin to automatically create the index.html file in the /dist directory.
 
-//npm install --save lodash (optional)
-const { template } = require('lodash');
+// npm install --save lodash (optional)
+// const { template } = require('lodash');
 
 module.exports = {
+  // webpack mode, entry and output config:
+  // in package.json add (under) "scripts":{ "build": "webpack"}
+  // Now, in the CLI run: [ $ npm run build ] to build your source.
+  mode: 'development',
+  entry: { mymain: myPath.resolve(__dirname, 'src/index.js') },
+  output: {
+    path: myPath.resolve(__dirname, 'dist'),
+    filename: '[name].bundle.js',
+    clean: true,
+  },
 
-    //webpack mode, entry and output config:
-    //in package.json add (under) "scripts":{ "build": "webpack"}
-    //Now, in the CLI run: [ $ npm run build ] to build your source.
-    mode: 'development',
-    entry: {
-        mymain: myPath.resolve(__dirname,'src/index.js')},
-    output: {
-        path: myPath.resolve(__dirname,'dist'),
-        filename:'[name].bundle.js',
-        clean:true
-    },
-
-    //webpack css loaders (style-loder, css-loader) config:
-    //npm install --save-dev style-loader css-loader
-    module:{
-        rules:[
-            {
-                test: /\.css$/i,
-                use:['style-loader','css-loader'],
-            }
-        ],
-    },
-
-    //webpack Html plugin for auto generating HTML under dist
-    //npm install --save-dev html-webpack-plugin
-    plugins: [
-        new HtmlWebpackPlugin ({
-            template : './src/index.html'
-        }),
+  // webpack css loaders (style-loder, css-loader) config:
+  // npm install --save-dev style-loader css-loader
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
     ],
+  },
 
-    //webpack dev server to setup local dev server
-    //npm install --save-dev webpack-dev-server 
-    //And, in package.json add (under) "scripts": { start: "webpack serve --open"}
-    //Now, in the CLI run: [ $ npm start ] to start the local dev server.
-    devServer: {
-        static: './dist',
-    },
+  // webpack Html plugin for auto generating HTML under dist
+  // npm install --save-dev html-webpack-plugin
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
 
-    optimization: {
-        runtimeChunk: 'single',
-    },
+  // webpack dev server to setup local dev server
+  // npm install --save-dev webpack-dev-server
+  // And, in package.json add (under) "scripts": { start: "webpack serve --open"}
+  // Now, in the CLI run: [ $ npm start ] to start the local dev server at: http://localhost:8080/.
+  devServer: {
+    static: './dist',
+  },
 
+  optimization: {
+    runtimeChunk: 'single',
+  },
 
-}
+};
