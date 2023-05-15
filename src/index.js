49e0dd4cdf5ea11c9c1c5f5b48e0a76585f744df
porag-m06 <porag.m06@gmail.com>
2023-05-15 21:16:26 +0600

    webpack img preprocessing solved with the icons

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
-//import './style.css';
-//import refresh from '../assets/rotate-solid.svg';
-//import arrowD from '../assets/arrow-turn-down-left.png'
-//import trashT from '../assets/trash-can-regular.svg'
-import TodoTasks from "./todo.js"
+import TodoTasks from './todo.js';
+import './style.css';
 
-// const refreshImg = document.getElementById('refresh');
-// refreshImg.src = refresh;
-// const arrowImg = document.getElementById('arrow-d-l');
-// arrowImg.src = arrowD;
-// const trashImg = document.getElementById('arrow-d-l');
-// trashImg.src = trashT;
+import refresh from '../assets/rotate-solid.svg';
+import arrowD from '../assets/arrow-turn-down-left.png';
+
+const refreshImg = document.getElementById('refresh');
+const arrowImg = document.getElementById('arrow-d-l');
+
+refreshImg.src = refresh;
+arrowImg.src = arrowD;
 
 const todoForm = document.querySelector('#task-form');
 const clearList = document.querySelector('.btn');
@@ -19,21 +18,17 @@ if (todos.getLocalStorage().length > 0) {
   todos.showTodoList();
 }
 
-
-["keypress", "submit"].forEach((item) => {
+['keypress', 'submit'].forEach((item) => {
   todoForm.addEventListener(item, (event) => {
-    if (event.key === "Enter" || event.type === "submit") {
-      //todoFormlistener(event);
+    if (event.key === 'Enter' || event.type === 'submit') {
       event.preventDefault();
-      const taskDescription = document.querySelector('.new-item').value;
-      const newTask = new TodoTasks(taskDescription, false, (new TodoTasks()).getLocalStorage().length);
+      const tDescrip = document.querySelector('.new-item').value;
+      const newTask = new TodoTasks(tDescrip, false, (new TodoTasks()).getLocalStorage().length);
       newTask.AddTodo();
       newTask.showTodoList();
       todoForm.reset();
     }
-  })
+  });
 });
 
-
 clearList.addEventListener('click', () => (new TodoTasks()).emptyList());
-
--- src/todo.js
+++ src/todo.js
@@ -1,85 +1,87 @@
+import trashImgSrc from '../assets/trash-can-regular.svg';
+
 export default class TodoTasks {
-    constructor(description, completed, index) {
-        this.description = description;
-        this.completed = completed;
-        this.index = index;
-        this.taskList = JSON.parse(localStorage.getItem('tasks')) || [];
-    }
+  constructor(description, completed, index) {
+    this.description = description;
+    this.completed = completed;
+    this.index = index;
+    this.taskList = JSON.parse(localStorage.getItem('tasks')) || [];
+  }
 
     AddTodo = () => {
-        const description = this.description;
-        const completed = this.completed;
-        const index = this.index;
-        this.taskList.push({ description, completed, index });
-        localStorage.setItem('tasks', JSON.stringify(this.taskList))
+      const { description } = this;
+      const { completed } = this;
+      const { index } = this;
+      this.taskList.push({ description, completed, index });
+      localStorage.setItem('tasks', JSON.stringify(this.taskList));
     }
 
     showTodoList = () => {
-        this.taskList.sort((a, b) => a.index - b.index);
+      this.taskList.sort((a, b) => a.index - b.index);
 
-        const list = document.querySelector('.list');
-        list.innerHTML = '';
+      const list = document.querySelector('.list');
+      list.innerHTML = '';
 
-        for (let i = 0; i < this.taskList.length; i += 1) {
-            const listItem = document.createElement('li');
-            listItem.className = 'hbb';
-            listItem.classList.add('added-task');
-            listItem.id = `${i}`
-            listItem.innerHTML = `<input type="checkbox" class="check${i}" name="" id="" value="">
+      for (let i = 0; i < this.taskList.length; i += 1) {
+        const listItem = document.createElement('li');
+        listItem.className = 'hbb';
+        listItem.classList.add('added-task');
+        listItem.id = `${i}`;
+        listItem.innerHTML = `<input type="checkbox" class="check${i}" name="" id="" value="">
             <input type="text" class="content content${i}" name="" id="${i}" value ="${this.taskList[i].description}">
-            <button id="trash-btn"><img id="trash-can" src='../assets/trash-can-regular.svg' alt="trash icon"></button>`;            
-            list.appendChild(listItem);
-            
-            const checkbox = document.querySelector(`.check${i}`);
-            checkbox.addEventListener('change',()=>{
-                if(checkbox.checked){
-                    document.querySelector(`.content${i}`).classList.add('completed');
-                }else{
-                    document.querySelector(`.content${i}`).classList.remove('completed');
-                }
-            })
-        };
+            <button id="trash-btn"><img id="trash-can" src="${trashImgSrc}" alt="trash icon"></button>`;
+        list.appendChild(listItem);
 
-        const tasks = document.querySelectorAll('.added-task');
-        const taskTrashBtns = document.querySelectorAll('#trash-btn');
-        this.removeTask(tasks, taskTrashBtns);
+        const checkbox = document.querySelector(`.check${i}`);
+        checkbox.addEventListener('change', () => {
+          if (checkbox.checked) {
+            document.querySelector(`.content${i}`).classList.add('completed');
+          } else {
+            document.querySelector(`.content${i}`).classList.remove('completed');
+          }
+        });
+      }
 
-        const taskDescriptions = document.querySelectorAll('.content');
-        this.saveOnChangeTask(taskDescriptions);
+      const tasks = document.querySelectorAll('.added-task');
+      const taskTrashBtns = document.querySelectorAll('#trash-btn');
+      this.removeTask(tasks, taskTrashBtns);
+
+      const taskDescriptions = document.querySelectorAll('.content');
+      this.saveOnChangeTask(taskDescriptions);
     }
 
     removeTask = (tasks, taskTrashBtns) => {
-        for (let i = 0; i < tasks.length; i += 1) {
-            taskTrashBtns[i].addEventListener('click', () => {
-                this.taskList.splice(tasks[i].getAttribute('id'), 1);
-                this.taskList.sort((a, b) => a.index - b.index);
-                this.resetIndex();
-                localStorage.setItem('tasks', JSON.stringify(this.taskList));
-                this.showTodoList();
-            })
-        };
+      for (let i = 0; i < tasks.length; i += 1) {
+        taskTrashBtns[i].addEventListener('click', () => {
+          this.taskList.splice(tasks[i].getAttribute('id'), 1);
+          this.taskList.sort((a, b) => a.index - b.index);
+          this.resetIndex();
+          localStorage.setItem('tasks', JSON.stringify(this.taskList));
+          this.showTodoList();
+        });
+      }
     }
 
-    saveOnChangeTask = (taskDescriptions)=>{
-        taskDescriptions.forEach(t => {
-            t.addEventListener('change',()=>{
-                this.taskList[t.getAttribute('id')].description = t.value;
-                localStorage.setItem('tasks', JSON.stringify(this.taskList));
-            })
+    saveOnChangeTask = (taskDescriptions) => {
+      taskDescriptions.forEach((t) => {
+        t.addEventListener('change', () => {
+          this.taskList[t.getAttribute('id')].description = t.value;
+          localStorage.setItem('tasks', JSON.stringify(this.taskList));
         });
+      });
     }
 
     getLocalStorage = () => this.taskList;
 
     emptyList = () => {
-        this.taskList = [];
-        localStorage.setItem('tasks', JSON.stringify(this.taskList));
-        this.showTodoList();
+      this.taskList = [];
+      localStorage.setItem('tasks', JSON.stringify(this.taskList));
+      this.showTodoList();
     }
 
-    resetIndex = ()=>{
-        for(let i = 0; i< this.taskList.length; i+=1){
-            this.taskList[i].index = i;
-        }
+    resetIndex = () => {
+      for (let i = 0; i < this.taskList.length; i += 1) {
+        this.taskList[i].index = i;
+      }
     }
 }
